4d72d0f5e28e4f81767128bbbdd7c1529ec9947e
br0therbee <298185117@qq.com>
2020-10-03 11:56:35 +0800

    no message

--- hunter/videos/tencent_video/decrypt/get_download_url.py
+++ hunter/videos/tencent_video/decrypt/get_download_url.py
@@ -14,7 +14,7 @@ import execjs
 from utils import RequestManager, LogManager, ciphers
 from hunter.videos import ParseError, CookieError, DownloadType, Provider
 from hunter.videos.tencent_video import get_cvid, VideoType
-from hunter.videos.tencent_video.decrypt.privacy import get_tkn, get_unid
+from privacy import get_tkn, get_unid
 from hunter.videos.tencent_video.login.mock import QQLogin
 
 logger = LogManager('video_tencent_video_get_download_url').file()
